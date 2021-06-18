7a78a7b760b84e878145186818a6fe0588d919ca
Ikhlas Asif <anasikhlas2001@gmail.com>
2021-06-18 19:40:37 +0300

    Download videos in different formats and custom file name, Now you can also download videos from YT Shorts

--- app.js
+++ app.js
@@ -9,21 +9,37 @@ const app = express();
 //   	.pipe(fs.createWriteStream('video.mp4'))
 // 	res.send("Hello")
 // })
- 
+
 // functions
 
 const convertUrl = (url) => {
+		
+	let newUrlArray;
+
 	if(url.includes("youtu.be")){
-		let newUrlArray = url.split("https://youtu.be/")
+
+	 newUrlArray = url.split("https://youtu.be/")
+	return `https://www.youtube.com/watch?v=${newUrlArray[1]}`
+
+	}else
+	 if(url.includes("https://www.youtube.com/shorts/")){
+
+		 newUrlArray = url.split("https://www.youtube.com/shorts/")
 		return `https://www.youtube.com/watch?v=${newUrlArray[1]}`
-	}else{
+	}
+	else if(url.includes("https://www.youtube.com/watch?v")){
 		return url
 	}
+	else{
+		return "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+	}
+	// youtube shorts url
+	// https://www.youtube.com/shorts/lZub2xQNYxo
 }
 
 // videos
 app.get("/video", async(req,res)=>{
-	const videoId = convertUrl(req.query.videoId)
+	const videoId = convertUrl(req.query.videoId,res)
 	let info = await ytdl.getInfo(videoId)
 	res.json(info)
 })
@@ -35,5 +51,24 @@ app.get("/audio", async(req,res)=>{
 	let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
 	res.json(audioFormats)
 })
+
+// Download video with custom title and quality
+app.get("/download", async(req,res)=>{
+
+	const {itag,title,type} = req.query
+	const videoId = convertUrl(req.query.videoId)
+
+	if(type === "mp4"){
+		res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
+		res.header('Content-Type', 'video/mp4');
+	}else if(type === "mp3"){
+		res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+		res.header('Content-Type', 'audio/mp3');
+	}
+
+	ytdl(videoId, { filter: format => format.itag === 18 }).pipe(res); 
+	
+})
+
 
 app.listen(4000,console.log("working..."))
